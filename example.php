18c5bb5c80b1a181fe295c01b3f4b12f238f41fd
bekhab <zombasi113@gmail.com>
2016-07-12 02:08:44 +0500

    Update example.php

--- example.php
+++ example.php
@@ -11,10 +11,10 @@
 	 */
 
 	// Step 1: Declare new NexmoMessage.
-	$nexmo_sms = new NexmoMessage('api_key', 'api_secret');
+	$nexmo_sms = new NexmoMessage('65cf5398', '6a1f5ca44d2f2f01');
 
 	// Step 2: Use sendText( $to, $from, $message ) method to send a message. 
-	$info = $nexmo_sms->sendText( '+447234567890', 'MyApp', 'Hello!' );
+	$info = $nexmo_sms->sendText( '+923317878783', '0333777777', 'Hello!' );
 
 	// Step 3: Display an overview of the message
 	echo $nexmo_sms->displayOverview($info);
